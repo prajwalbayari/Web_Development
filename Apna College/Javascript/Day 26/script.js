9f097b88d324275e7da4308396378a20088f30f3
prajwalbayari <prajwalbayari4@gmail.com>
2024-10-27 18:59:11 +0530

    facts using axios

--- Apna College/Javascript/Day 26/script.js
+++ Apna College/Javascript/Day 26/script.js
@@ -1,4 +1,24 @@
+let b=document.querySelector('button');
+let p=document.querySelector('p');
+b.addEventListener("click",function(){
+    getFacts();
+})
+
 let url="https://catfact.ninja/fact";
+
+async function getFacts(){
+    try{
+        let res=await axios.get(url);
+        console.log(res);
+        let data=res.data.fact;
+        p.innerText=data;
+        console.log(data);
+    }catch(e){
+        console.log("error- ",e);
+        p.innerText="No fact found";
+    }
+}
+
 // fetch(url)
 // .then(response=>{
 //     console.log(response);
@@ -18,8 +38,16 @@ let url="https://catfact.ninja/fact";
 //     console.log(err);
 // })
 
-async function getFacts(){
-    let res=await fetch(url);
-    let data=await res.json();
-    console.log(data.fact);
-}
+// async function getFacts(){
+//     try{
+//         let res=await fetch(url);
+//         let data=await res.json();
+//         console.log(data.fact);
+
+//         let res2=await fetch(url);
+//         let data2=await res2.json();
+//         console.log(data2.fact);
+//     }catch(e){
+//         console.log("error- ",e);
+//     }
+// }
