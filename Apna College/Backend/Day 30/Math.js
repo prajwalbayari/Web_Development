ac90bfd82e8bf805ea124d08ae3b4ce6e6ce5f68
prajwalbayari <prajwalbayari4@gmail.com>
2024-10-29 00:53:18 +0530

    Intoduction to Nodejs

---
+++ Apna College/Backend/Day 30/Math.js
@@ -0,0 +1,29 @@
+// const sum=(a,b) => a+b; 
+// const mul=(a,b) => a*b; 
+
+// const g=9.8,pi=3.142;
+
+// let obj={
+//     sum:sum,mul:mul,g:g,pi:pi
+// }
+// module.exports=obj;
+
+
+// -------------OR--------------------
+
+// const sum=(a,b) => a+b; 
+// const mul=(a,b) => a*b; 
+
+// const g=9.8,pi=3.142;
+
+// module.exports={
+//     sum:sum,mul:mul,g:g,pi:pi
+// }
+
+// -------------OR--------------------
+
+module.exports.sum=(a,b) => a+b; 
+module.exports.mul=(a,b) => a*b; 
+
+module.exports.g=9.8;
+module.exports.pi=3.142;
---
+++ Apna College/Backend/Day 30/script.js
@@ -0,0 +1,18 @@
+//Trial
+
+// for(let i=0;i<10;i++){
+//     console.log("Hello ",i);
+// }
+
+// console.log("Bye!!")
+
+// let args=process.argv;
+
+// for(let i=2;i<args.length;i++)
+//     console.log("Hello welcome to ",args[i]);
+
+const some=require("./Math")
+
+console.log(some.pi);
+console.log(some.g);
+console.log(some.mul(10,2));
