9fc6b57f634c790d7bc577cf5fe2c3e2a4c9e015
prajwalbayari <prajwalbayari4@gmail.com>
2024-11-19 19:41:46 +0530

    Added data

--- Jonas Schmedtmann/Mongoose/server.js
+++ Jonas Schmedtmann/Mongoose/server.js
@@ -1,7 +1,45 @@
+const mongoose=require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB=process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD)
+mongoose.connect(DB)
+.then(()=>{
+  console.log("Database connection successfull\n");
+})
+
+const tourSchema=new mongoose.Schema({
+  name:{
+    type:String,
+    required:[true,'Tour must have a name'],
+    unique:true
+  },
+  rating:{
+    type:Number,
+    default:4.5,
+  },
+  price:{
+    type:Number,
+    required:[true,'Tour must have a price']
+  }
+})
+
+const Tour=mongoose.model('Tour',tourSchema);
+
+const testTour=new Tour({
+  name:'The park camper',
+  price:4000
+})
+
+testTour.save()
+.then(doc=>{
+  console.log(doc);
+})
+.catch((err)=>{
+  console.log('ERROR 🔪🔪🔪')
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
