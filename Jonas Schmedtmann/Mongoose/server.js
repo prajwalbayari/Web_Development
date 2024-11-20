1d3d2c93e1f7d891f235bab34f3289d56f0e42a1
prajwalbayari <prajwalbayari4@gmail.com>
2024-11-20 22:12:26 +0530

    Created tourModel

--- Jonas Schmedtmann/Mongoose/server.js
+++ Jonas Schmedtmann/Mongoose/server.js
@@ -10,37 +10,6 @@ mongoose.connect(DB)
   console.log("Database connection successfull\n");
 })
 
-const tourSchema=new mongoose.Schema({
-  name:{
-    type:String,
-    required:[true,'Tour must have a name'],
-    unique:true
-  },
-  rating:{
-    type:Number,
-    default:4.5,
-  },
-  price:{
-    type:Number,
-    required:[true,'Tour must have a price']
-  }
-})
-
-const Tour=mongoose.model('Tour',tourSchema);
-
-const testTour=new Tour({
-  name:'The park camper',
-  price:4000
-})
-
-testTour.save()
-.then(doc=>{
-  console.log(doc);
-})
-.catch((err)=>{
-  console.log('ERROR 🔪🔪🔪')
-})
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
