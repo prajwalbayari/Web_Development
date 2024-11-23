3ca1f5afd5f26cd249e6bf3266c59b79162e27d3
prajwalbayari <prajwalbayari4@gmail.com>
2024-11-23 19:49:04 +0530

    Refactoring

--- Jonas Schmedtmann/Mongoose/controllers/tourController.js
+++ Jonas Schmedtmann/Mongoose/controllers/tourController.js
@@ -1,64 +1,70 @@
-const Tour= require('../models/tourModel')
+const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
-exports.aliasTopTours = (req,res,next)=>{
-  req.query.limit= '5';
-  req.query.sort='-ratingsAverage,price';
-  req.query.fields='name,price,ratingsAverage,summary,difficulty';
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
   next();
-}
+};
 
-exports.getAllTours =async (req, res) => {
-  try{
-    
+exports.getAllTours = async (req, res) => {
+  try {
     //1A)Filtering
-    
-    const queryObj={...req.query};
-    const excludedFields=['page','sort','limit','fields'];
-    excludedFields.forEach(el=>delete queryObj[el]);
-    console.log(req.query,queryObj);
 
-    //1B)Advanced filtering
+    // const queryObj={...req.query};
+    // const excludedFields=['page','sort','limit','fields'];
+    // excludedFields.forEach(el=>delete queryObj[el]);
+    // console.log(req.query,queryObj);
 
-    let queryStr=JSON.stringify(queryObj);
-    queryStr=queryStr.replace(/\bgte|gt|lte|lt\b/g, match=>`$${match}`);
-    console.log(JSON.parse(queryStr));
+    // //1B)Advanced filtering
 
-    let query=Tour.find(JSON.parse(queryStr));
+    // let queryStr=JSON.stringify(queryObj);
+    // queryStr=queryStr.replace(/\bgte|gt|lte|lt\b/g, match=>`$${match}`);
+    // console.log(JSON.parse(queryStr));
 
-    // 2)Sort 
+    // let query=Tour.find(JSON.parse(queryStr));
 
-    if(req.query.sort){
-      query=query.sort(req.query.sort);
-    }else{
-      query=query.sort('-createdAt');
-    }
+    // 2)Sort
+
+    // if(req.query.sort){
+    //  const sortBy=req.query.sort.split(',').join(' ');
+    //  query=query.sort(sortBy);
+    // }else{
+    //   query=query.sort('-createdAt');
+    // }
 
     //3)Field limiting
-    if(req.query.fields){
-      const fields=req.query.fields.split(',').join(' ');
-      query=query.select(fields);
-    }else{
-      query=query.select('');
-    }
+    // if(req.query.fields){
+    //   const fields=req.query.fields.split(',').join(' ');
+    //   query=query.select(fields);
+    // }else{
+    //   query=query.select('');
+    // }
 
     //4)Pagination
-    const page=req.query.page*1 || 1; //Converts string to number
-    const limit=req.query.limit*1 || 100;
-    const skip=(page-1)*limit;
-    console.log(skip);
-    query=query.skip(skip).limit(limit);
-    if(req.query.page){
-      const numTours=await Tour.countDocuments();
-      if(skip>numTours)
-        throw new Error('This page does not exist');
-    }
+    // const page=req.query.page*1 || 1; //Converts string to number
+    // const limit=req.query.limit*1 || 100;
+    // const skip=(page-1)*limit;
+    // console.log(skip);
+    // query=query.skip(skip).limit(limit);
+    // if(req.query.page){
+    //   const numTours=await Tour.countDocuments();
+    //   if(skip>numTours)
+    //     throw new Error('This page does not exist');
+    // }
 
     //EXECUTE
 
     // const tours =await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
 
     // const tours =await Tour.find(query);
-    const tours=await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -66,55 +72,55 @@ exports.getAllTours =async (req, res) => {
         tours
       }
     });
-  }catch(err){
+  } catch (err) {
     res.status(400).json({
-      status:'failure',
-      message:err.message
-    }); 
+      status: 'failure',
+      message: err.message
+    });
   }
 };
 
-exports.getTour =async (req, res) => {
-  try{
-    const tour=await Tour.findById(req.params.id);
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
     res.status(200).json({
-      status:"Success",
-      data:{
+      status: 'Success',
+      data: {
         tour
       }
-    })
-  }catch(err){
+    });
+  } catch (err) {
     res.status(404).json({
-      status:"failure",
-      message:"Could not fetch document"
-    })
+      status: 'failure',
+      message: 'Could not fetch document'
+    });
   }
 };
 
 exports.createTour = async (req, res) => {
-  try{
-    const newTour=await Tour.create(req.body).then()
-  
+  try {
+    const newTour = await Tour.create(req.body).then();
+
     res.status(201).json({
       status: 'success',
       data: {
         tour: newTour
       }
     });
-  }catch(err){
+  } catch (err) {
     res.status(404).json({
-      status:'fail',
-      message:err
+      status: 'fail',
+      message: err
     });
   }
 };
 
 exports.updateTour = async (req, res) => {
-  try{
-    const tour=await Tour.findByIdAndUpdate(req.params.id,req.body,{
-      new:true,
-      runValidators:true
-    })
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
 
     res.status(200).json({
       status: 'success',
@@ -122,26 +128,26 @@ exports.updateTour = async (req, res) => {
         tour: tour
       }
     });
-  }catch(err){
+  } catch (err) {
     res.status(404).json({
-      status:'failure',
-      message:'Could not update tour due to some error'
-    })
+      status: 'failure',
+      message: 'Could not update tour due to some error'
+    });
   }
 };
 
-exports.deleteTour =async (req, res) => {
-  try{
-    const tour=await Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
 
     res.status(204).json({
       status: 'success',
       data: null
     });
-  }catch(err){
+  } catch (err) {
     res.status(404).json({
-      status:'failure',
+      status: 'failure',
       message: 'Wrong id'
-    })
+    });
   }
 };
