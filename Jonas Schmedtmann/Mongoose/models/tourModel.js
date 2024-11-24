8dbe30d60d55c61c0403d9772dd62ab73439b96f
prajwalbayari <prajwalbayari4@gmail.com>
2024-11-24 16:38:12 +0530

    Middlewares

--- Jonas Schmedtmann/Mongoose/models/tourModel.js
+++ Jonas Schmedtmann/Mongoose/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const slugify=require('slugify');
 
 const tourSchema=new mongoose.Schema({
     name:{
@@ -7,6 +8,7 @@ const tourSchema=new mongoose.Schema({
       unique:true,
       trim:true
     },
+    slug:String,
     duration:{
       type:Number,
       required:[true,'A tour must have duration']
@@ -51,7 +53,11 @@ const tourSchema=new mongoose.Schema({
       default:Date.now(),
       select:false
     },
-    startDates:[Date]
+    startDates:[Date],
+    secretTour:{
+      type:Boolean,
+      default:false
+    }
   },{
     toJSON: {virtuals:true},
     toObject: {virtuals:true}
@@ -59,6 +65,47 @@ const tourSchema=new mongoose.Schema({
 
   tourSchema.virtual('durationWeeks').get(function(){
     return this.duration/7;
+  })
+
+  //MIDDLEWARS
+
+  //1)DOCUMENT MIDDLEWARE: runs before .save() and .create()
+
+  tourSchema.pre('save',function(next){
+    this.slug=slugify(this.name,{lower:true});
+    next();
+  });
+  
+  // tourSchema.pre('save',function(next){
+  //   console.log('Will save document')
+  //   next();
+  // }); 
+
+  // tourSchema.post('save',function(doc,next){
+  //   console.log(doc);
+  //   next();
+  // })
+
+  // 2)QUERY MIDDLEWARE
+
+  // tourSchema.pre('find',function(next){ //Executes oonly for find()
+  tourSchema.pre('/^find/',function(next){     //Executes for all methods starting with find
+    this.find({ secretTour:{$ne:true}});
+    this.start=Date.now();
+    next();
+  })
+
+  tourSchema.post('/^find/',function(docs,next){
+    console.log(`Query took ${Date.now() - this.start} milliseconds`);
+    next();
+  })
+
+  //3)AGGREGATION MIDDLEWARE
+
+  tourSchema.pre('aggregate',function(next){
+    console.log(this.pipeline());
+    this.pipeline().unshift({$match :{ secretTour: {$ne:true}}})
+    next();
   })
   
   const Tour=mongoose.model('Tour',tourSchema);
