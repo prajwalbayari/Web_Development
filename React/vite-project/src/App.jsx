901b15e3e8c2f66c30ddc9348b91ecbf3f9c0c9e
prajwalbayari <prajwalbayari4@gmail.com>
2024-11-26 22:03:26 +0530

    Basics

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,5 +1,24 @@
+import React from "react";
+
 function App() {
-  return "Hello world";
+  // return React.createElement("h1", { id: "5" }, "Nirma");
+
+  // return(
+  //   <h1 id="5" className="blue" style={{background:"red"}}>
+  //     Hello world <span>Bye</span>
+  //   </h1>
+  // )
+
+  // return(
+  //   <label><h1>{2+2}</h1></label>
+  // )
+
+  return(
+    <div className="large" id="largeDiv">
+      <label htmlFor="inputId">Nirma</label>
+      <input type="text" id="inputId" type="number" defaultValue={3}/>
+    </div>
+  )
 }
 
 export default App;
